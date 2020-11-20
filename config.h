00853d32ba462274609d79e8f9f31a9a93140142
Tadas Nevinskas <tadasnevinskas@gmail.com>
2020-11-20 22:02:36 +0200

    Add pcmanfm start shortcut

--- config.h
+++ config.h
@@ -66,6 +66,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[] = { "st", NULL };
 static const char *browser[] = { "firefox", NULL };
+static const char *filemng[] = { "pcmanfm", NULL };
 static const char *incbckl[] = { "xbacklight", "+10", "-time", "1", "-steps", "100", NULL };
 static const char *decbckl[] = { "xbacklight", "-10", "-time", "1", "-steps", "100", NULL };
 static const char *mute[]    = { "amixer", "-q", "-D", "pulse", "sset", "Master", "toggle", NULL };
@@ -80,6 +81,7 @@ static Key keys[] = {
 	{ MODKEY,             XK_r,                    spawn,          {.v = dmenucmd } },
 	{ MODKEY,             XK_Return,               spawn,          {.v = termcmd } },
 	{ MODKEY,             XK_grave,                spawn,          {.v = browser } },
+	{ 0,                  XK_Pause,                spawn,          {.v = filemng } },
 	{ MODKEY,             XK_b,                    togglebar,      {0} },
 	{ MODKEY,             XK_j,                    focusstack,     {.i = +1 } },
 	{ MODKEY,             XK_k,                    focusstack,     {.i = -1 } },
